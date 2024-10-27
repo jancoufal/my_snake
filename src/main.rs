07f483edbea3501c2f122fb8687ffafa531894e5
Coufal, Jan <jan.coufal@oracle.com>
2024-10-27 23:25:19 +0100

    gl leak (broken)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use graphics::math::Scalar;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+use std::cell::RefCell;
+
+static CELL_CODE_INIT_HEAD: i32 = 1;
+static CELL_CODE_EMPTY: i32 = 0;
+static CELL_CODE_BORDER: i32 = -1;
+static CELL_CODE_FOOD: i32 = -2;
+
+
 #[derive(Debug, Copy, Clone)]
 struct Point2D {
     x: i32,
@@ -25,7 +44,28 @@ enum CellType {
 
 #[derive(Debug, Copy, Clone)]
 struct Cell {
-    cell_type: CellType,
+    int_code: i32,
+}
+
+impl Cell {
+    fn new_border() -> Cell { Cell { int_code: CELL_CODE_BORDER } }
+    fn new_empty() -> Cell { Cell { int_code: CELL_CODE_EMPTY } }
+    fn new_food() -> Cell { Cell { int_code: CELL_CODE_FOOD } }
+    fn new_head() -> Cell { Cell { int_code: CELL_CODE_INIT_HEAD } }
+
+    fn get_type(&self, snake_length: i32) -> CellType {
+        match self.int_code {
+            // CELL_CODE_EMPTY => CellType::Empty,
+            // CELL_CODE_BORDER => CellType::Border,
+            // CELL_CODE_FOOD => CellType::Food,
+            0 => CellType::Empty,
+            -1 => CellType::Border,
+            -2 => CellType::Food,
+            _ if self.int_code == snake_length => CellType::SnakeHead,
+            1 => CellType::SnakeTail, // todo: not really true
+            _ => CellType::SnakeBody,
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -45,7 +85,8 @@ enum GameState {
 #[derive(Debug)]
 struct Game {
     state: GameState,
-    field: Vec<Vec<Cell>>,
+    field: Vec<Vec<RefCell<Cell>>>,
+    // snake: Vec<Weak<RefCell<Cell>>>,
     direction: Direction,
     snake_length: usize,
     step_count: usize,
@@ -53,22 +94,23 @@ struct Game {
 
 impl Game {
     fn new(cols: usize, rows: usize) -> Game {
-        let mut field = vec![vec![Cell { cell_type: CellType::Empty }; cols]; rows];
 
-        // make border
-        for (y, row) in field.iter_mut().enumerate()
-        {
-            for (x, cell) in row.iter_mut().enumerate()
-            {
-                if y == 0 || y == rows - 1 || x == 0 || x == cols - 1
-                {
-                    cell.cell_type = CellType::Border;
-                }
+        let mut field: Vec<Vec<RefCell<Cell>>> = Vec::new();
+        for r in 0..rows {
+            let mut cell_row: Vec<RefCell<Cell>> = Vec::new();
+            for c in 0..cols {
+                let new_cell = match (c, r) {
+                    (0, _) => Cell::new_border(),
+                    (_, _) if c == cols-1 => Cell::new_border(),
+                    (_, 0) => Cell::new_border(),
+                    (_, _) if r == rows-1 => Cell::new_border(),
+                    (_, _) if c == cols/2 && r == rows/2 => Cell::new_head(),
+                    (_, _) => Cell::new_empty(),
+                };
+                cell_row.push(RefCell::new(new_cell));
             }
+            field.push(cell_row);
         }
-
-        let (mid_x, mid_y) = (cols / 2, rows / 2);
-        field[mid_y][mid_x].cell_type = CellType::SnakeHead;
 
         Game {
             state: GameState::Paused,
@@ -85,22 +127,98 @@ impl Game {
     }
 }
 
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64,  // Rotation for the square.
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs, game: &Game) {
+        use graphics::*;
+
+        const BROWN: [f32; 4] = [1.0, 0.5, 0.2, 1.0];
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const DARK: [f32; 4] = [0.1, 0.1, 0.1, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const YELLOW: [f32; 4] = [1.0, 1.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            // for (y, field_row) in game.field.iter().enumerate() {
+            //     for (x, cell) in field_row.iter().enumerate() {
+                    let transform = c.transform
+                        // .trans(x as Scalar + 25., y as Scalar + 25.)
+                        .trans(25., 25.)
+                        ;
+
+                    // let color = match cell.borrow().get_type(game.snake_length as i32) {
+                    //     CellType::Empty => DARK,
+                    //     CellType::Border => BROWN,
+                    //     CellType::SnakeHead => GREEN,
+                    //     CellType::SnakeBody => GREEN,
+                    //     CellType::SnakeTail => GREEN,
+                    //     CellType::Food => RED,
+                    // };
+
+                    rectangle(RED, square, transform, gl);
+                // }
+            // }
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += 2.0 * args.dt;
+    }
+}
+
 fn main() {
     let (cols, rows) = (10, 10);
+    let game = Game::new(cols, rows);
 
-    let mut game = Game::new(cols, rows);
+    // for col in game.field {
+    //     for cell in col {
+    //         print!("{}", match cell.cell_type {
+    //             CellType::Empty => " ".to_string(),
+    //             CellType::Border => "#".to_string(),
+    //             CellType::SnakeHead => "@".to_string(),
+    //             CellType::SnakeBody => "*".to_string(),
+    //             CellType::SnakeTail => ".".to_string(),
+    //             CellType::Food => "Q".to_string(),
+    //         });
+    //     }
+    //     println!();
+    // }
 
-    for col in game.field {
-        for cell in col {
-            print!("{}", match cell.cell_type {
-                CellType::Empty => " ".to_string(),
-                CellType::Border => "#".to_string(),
-                CellType::SnakeHead => "@".to_string(),
-                CellType::SnakeBody => "*".to_string(),
-                CellType::SnakeTail => ".".to_string(),
-                CellType::Food => "Q".to_string(),
-            });
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V4_5;
+
+    // Create a Glutin window.
+    let mut window: Window = WindowSettings::new("my-snake", [500, 500])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args, &game);
         }
-        println!();
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
     }
 }
